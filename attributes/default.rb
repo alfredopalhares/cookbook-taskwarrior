4d2443343546d5956c78ffabdb022509caf7b291
Alfredo Palhares <masterkorp@masterkorp.net>
2013-12-26 18:31:34 +0000

    Server attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,5 @@ default["taskwarrior"]["install_method"] = "package"
 default["taskwarrior"]["source"]["git_repository"] = "git://tasktools.org/task.git"
 default["taskwarrior"]["source"]["git_revision"] = "HEAD"
 
+default["taskwarrior"]["server"]["git_repository"] = "git://tasktools.org/taskd.git"
+default["taskwarrior"]["server"]["git_revision"] = "HEAD"
