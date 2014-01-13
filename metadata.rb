fd63964fa9f4dbd195e5b8f5e11ca6bf2a1175ca
Alfredo Palhares <masterkorp@masterkorp.net>
2014-01-13 16:27:24 +0000

    Update author and License details on metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'taskwarrior'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Alfredo Palhares'
+maintainer_email 'masterkorp@masterkorp.net'
+license          'MIT'
 description      'Installs/Configures taskwarrior'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
