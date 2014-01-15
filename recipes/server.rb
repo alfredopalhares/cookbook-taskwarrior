89f67a551c7952c30f7c5a7be44d9130453e4262
Alfredo Palhares <masterkorp@masterkorp.net>
2014-01-15 17:17:33 +0000

    Move the certificates generation to another recipe.

--- recipes/server.rb
+++ recipes/server.rb
@@ -79,41 +79,7 @@ if node["taskwarrior"]["server"]["initialized"]  == false then
   node.set["taskwarrior"]["server"]["initialized"] == true
 end
 
-package "gnutls-bin"
-
-bash "Generating CA key" do
-  user "root"
-  cwd node["taskwarrior"]["server"]["home"]
-  code <<-EOH
-    certtool --generate-privkey --outfile ca.key.pem
-  EOH
-  not_if {::File.exists?("#{node["taskwarrior"]["server"]["home"]}/ca.key.pem")}
-end
-
-template "#{node["taskwarrior"]["server"]["home"]}/ca.info" do
-  source "ca.info.erb"
-  owner "root"
-  group "root"
-  mode 00600
-  variables({
-    :organization => node["taskwarrior"]["server"]["organization"]
-  })
-  not_if {::File.exists?("#{node["taskwarrior"]["server"]["home"]}/ca.cert.pem")}
-  notifies :run, "bash[Generating CA Cert]", :immediately
-end
-
-bash "Generating CA Cert" do
-  user "root"
-  cwd node["taskwarrior"]["server"]["home"]
-  code <<-EOH
-    certtool --generate-self-signed \
-    --load-privkey ca.key.pem \
-    --template ca.info \
-    --outfile ca.cert.pem
-    rm ca.info
-  EOH
-  action :nothing
-end
+include_recipe "taskwarrior::certs"
 
 runit_service "taskd" do
   options({
