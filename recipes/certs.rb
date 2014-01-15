44feb18bc14baa9151d81c90b2dd8065c51d66a0
Alfredo Palhares <masterkorp@masterkorp.net>
2014-01-15 17:37:15 +0000

    Server key and cert generation.

--- recipes/certs.rb
+++ recipes/certs.rb
@@ -33,3 +33,40 @@ bash "Generating CA Cert" do
   EOH
   action :nothing
 end
+
+bash "Generating Server key" do
+  user "root"
+  cwd node["taskwarrior"]["server"]["home"]
+  code <<-EOH
+    certtool --generate-privkey --outfile server.key.pem
+  EOH
+  not_if {::File.exists?("#{node["taskwarrior"]["server"]["home"]}/server.key.pem")}
+end
+
+template "#{node["taskwarrior"]["server"]["home"]}/server.info" do
+  source "server.info.erb"
+  owner "root"
+  group "root"
+  mode 00600
+  variables({
+    :organization => node["taskwarrior"]["server"]["organization"],
+    :cn => node["ipaddress"]
+  })
+  not_if {::File.exists?("#{node["taskwarrior"]["server"]["home"]}/server.cert.pem")}
+  notifies :run, "bash[Generating Server Cert]", :immediately
+end
+
+bash "Generating Server Cert" do
+  user "root"
+  cwd node["taskwarrior"]["server"]["home"]
+  code <<-EOH
+    certtool --generate-certificate \
+    --load-privkey server.key.pem \
+    --load-ca-certificate ca.cert.pem \
+    --load-ca-privkey ca.key.pem \
+    --template server.info \
+    --outfile server.cert.pem
+    rm server.info
+  EOH
+  action :nothing
+end
