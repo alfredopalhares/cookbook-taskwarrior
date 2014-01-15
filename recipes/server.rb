8c39970e83a5f946f48e93c8ed1329adfbbcb9b8
Alfredo Palhares <masterkorp@masterkorp.net>
2014-01-15 16:26:08 +0000

    CA certificate generation.

--- recipes/server.rb
+++ recipes/server.rb
@@ -81,13 +81,38 @@ end
 
 package "gnutls-bin"
 
-bash "Generating CA" do
+bash "Generating CA key" do
   user "root"
   cwd node["taskwarrior"]["server"]["home"]
   code <<-EOH
     certtool --generate-privkey --outfile ca.key.pem
   EOH
   not_if {::File.exists?("#{node["taskwarrior"]["server"]["home"]}/ca.key.pem")}
+end
+
+template "#{node["taskwarrior"]["server"]["home"]}/ca.info" do
+  source "ca.info.erb"
+  owner "root"
+  group "root"
+  mode 00600
+  variables({
+    :organization => "Lol chef"
+  })
+  not_if {::File.exists?("#{node["taskwarrior"]["server"]["home"]}/ca.cert.pem")}
+  notifies :run, "bash[Generating CA Cert]", :immediately
+end
+
+bash "Generating CA Cert" do
+  user "root"
+  cwd node["taskwarrior"]["server"]["home"]
+  code <<-EOH
+    certtool --generate-self-signed \
+    --load-privkey ca.key.pem \
+    --template ca.info \
+    --outfile ca.cert.pem
+    rm ca.info
+  EOH
+  action :nothing
 end
 
 runit_service "taskd" do
